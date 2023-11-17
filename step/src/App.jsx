73bd656b7981bf85e10ac7bcc2c0ad6ae4154984
sueyanshin <sueyanshin@gmail.com>
2023-11-17 22:21:36 +0630

    eat-n-split

--- step/src/App.jsx
+++ step/src/App.jsx
@@ -43,23 +43,27 @@ const Steps = () => {
           <p className="message">
             Step {step}: {messages[step - 1]}
           </p>
-
           <div className="buttons">
-            <button
-              style={{ backgroundColor: " #7850f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: " #7850f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>{" "}
+            <Button bgColor="#7850f2" color="#fff" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button bgColor="#7850f2" color="#fff" onClick={handleNext}>
+              Next👉
+            </Button>
           </div>
         </div>
-      )}{" "}
+      )}
     </div>
   );
 };
+
+function Button({ bgColor, color, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: { bgColor }, color: { color } }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
